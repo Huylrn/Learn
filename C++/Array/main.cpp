26cf50490da2bdb5ae5b9313f372a49334584272
HuyNguyen <141661219+Huylrn@users.noreply.github.com>
2023-10-25 20:51:40 +0700

    up

--- C++/Array/main.cpp
+++ C++/Array/main.cpp
@@ -1,33 +1,7 @@
 #include <iostream>
 using namespace std;
-void input_array(int *arr, int n, int m)
-{
-    int sum = 0;
-    for (int i = 0; i < n * m; i++)
-    {
-        cin >> arr[i];
-    }
-    // n is the key
-    int index = 0; // save the index
-    for (int i = 0; i < n; i++)
-    {
+int main() {
 
-        for (int j = 0; j < m; j++)
-        {
-            sum += arr[index];
-            index++;
-        }
-        cout << sum << endl;
-        sum = 0;
-    }
-
-} // type value to array
-int main()
-{
-    // Multidimensional Array
-    int n, m;
-    cin >> n >> m;
-    int *arr = new int[n * m];
-    input_array(arr, n, m);
     return 0;
+
 }
