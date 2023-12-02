77e84ae90c2c6ddbfb5408211706a1aa435d0b7d
HuyNguyen <huuynguyendd@gmail.com>
2023-12-02 11:31:16 +0700

    none

--- C++/funn.cpp
+++ C++/funn.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <math.h>
+#include "funn.h"
 
 using namespace std;
 
-bool PrimeNumber(int a) {
-    if (a == 1) return false;
-    for (int i = 2; i <= sqrt(a); i++) {
-        if (a % i == 0) {
+bool PrimeNumber(int a)
+{
+    if (a == 1)
+        return false;
+    for (int i = 2; i <= sqrt(a); i++)
+    {
+        if (a % i == 0)
+        {
             return false;
         }
     }
     return true;
 } // check prime number
-void square() {
+void square()
+{
     int line = 5;
     int column = 5;
-    for (int i = 1; i <= column; i++) {
-        for (int j = 1; j <= line; j++) {
+    for (int i = 1; i <= column; i++)
+    {
+        for (int j = 1; j <= line; j++)
+        {
             cout << "|";
             cout << "1";
         }
@@ -24,13 +32,13 @@ void square() {
     }
 } // make a square 5x5
 
-int main() {
+int main()
+{
     // int s;
     // cout << "Sum s = ";
     // cin >> s;
 
     square();
-
 
     return 0;
 }
